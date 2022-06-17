654425c8861be25a36c9883f67c15011ef0be4d2
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-06-17 14:08:59 -0700

    co-working 6/17, toggleComplete is working?

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
-
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+import { useState } from 'react';
 
 const App = () => {
+  const [TASKS, setTasks] = useState([
+    {
+      id: 1,
+      title: 'Mow the lawn',
+      isComplete: false,
+    },
+    {
+      id: 2,
+      title: 'Cook Pasta',
+      isComplete: true,
+    },
+  ]);
+
+  const toggleComplete = (id) => {
+    // const [complete, setComplete] = useState(isComplete);
+    // const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+    const newTasks = [];
+    
+    for (const task of TASKS) {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete;
+      }
+      newTasks.push(task);
+    }
+    setTasks(newTasks);
+    console.log('toggleComplete was called');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>{<TaskList tasks={TASKS} toggleCompleteCallback={toggleComplete}/>}</div>
       </main>
     </div>
   );
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Task from './Task';
 import './TaskList.css';
 
-const TaskList = ({ tasks }) => {
+const TaskList = (props) => {
   const getTaskListJSX = (tasks) => {
     return tasks.map((task) => {
       return (
@@ -12,11 +12,13 @@ const TaskList = ({ tasks }) => {
           id={task.id}
           title={task.title}
           isComplete={task.isComplete}
+          // should this be task.toggle or tasks.toggle?
+          toggleCompleteCallback={props.toggleCompleteCallback}
         />
       );
     });
   };
-  return <ul className="tasks__list no-bullet">{getTaskListJSX(tasks)}</ul>;
+  return <ul className="tasks__list no-bullet">{getTaskListJSX(props.tasks)}</ul>;
 };
 
 TaskList.propTypes = {
@@ -26,7 +28,8 @@ TaskList.propTypes = {
       title: PropTypes.string.isRequired,
       isComplete: PropTypes.bool.isRequired,
     })
-  ).isRequired,
+  ),
+  toggleCompleteCallback: PropTypes.func.isRequired
 };
 
 export default TaskList;
