68bd2acd6fce8320137df47d1e1ca5bd1f6fda67
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-06-24 11:11:07 -0700

    Finished Wave 3

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ from the database when the React app is loaded
         console.log('get request');
         console.log(response);
         const newTasks = response.data.map((task) => {
-          // left = state, right = response data (use same attrib name)
+          // left = state, right = response data (use same attribute name)
           return {
             id: task.id,
             title: task.title,
@@ -37,30 +37,60 @@ from the database when the React app is loaded
 
 
   const toggleComplete = (id) => {
-    // const [complete, setComplete] = useState(isComplete);
-    // const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
-    const newTasks = [];
-    
+
+    // determines URL based on if task is complete
+    let checkTaskComplete = false;
+    let toggleCompleteURL = 'mark_complete';
+
     for (const task of TASKS) {
-      if (task.id === id) {
-        task.isComplete = !task.isComplete;
+      const newTask = {...task};
+      if (newTask.id === id) {
+        checkTaskComplete = newTask.isComplete;
       }
-      newTasks.push(task);
     }
-    setTasks(newTasks);
-    console.log('toggleComplete was called');
+      if (checkTaskComplete === true) {
+        toggleCompleteURL = 'mark_incomplete';
+      }
+    
+    // PATCH request to toggle task complete
+    axios
+      .patch(`${URL}/${id}/${toggleCompleteURL}`)
+      .then(() => {
+        const newTasks = [];
+        for (const task of TASKS) {
+          const newTask = {...task};
+          if (newTask.id === id) {
+            newTask.isComplete = !newTask.isComplete;
+          }
+          newTasks.push(newTask);
+        }
+        setTasks(newTasks);
+        console.log('toggleComplete was called');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    
   };
 
+  // Delete request to delete task
   const deleteTask = (id) => {
-    const newTasks = [];
+    axios
+      .delete(`${URL}/${id}`)
+      .then(() => {
+      const newTasks = [];
 
-    for (const task of TASKS) {
-      if (task.id !== id) {
-        newTasks.push(task);
+      for (const task of TASKS) {
+        if (task.id !== id) {
+          newTasks.push(task);
+        }
       }
-    setTasks(newTasks);
-    console.log('deleteTask was called');
-    }
+      setTasks(newTasks);
+      console.log('deleteTask was called');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   };
 
   return (
